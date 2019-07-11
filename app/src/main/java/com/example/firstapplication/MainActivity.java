025a0de21122bd5b0d9e8eeff944cf14406d555c
abogiegie <46108104+abogiegie@users.noreply.github.com>
2019-07-11 14:04:25 +0800

    CreateNewProject
    Create new project

---
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.firstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.test_button);
+        textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(textView.getText() + "ga");
+            }
+        });
+    }
+}
